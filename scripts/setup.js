ba2b26c501e02faf95df7bf64eb158357c38a1d8
Edgar R. Sandi <edgar.r.sandi@gmail.com>
2014-11-19 14:17:44 -0200

    WIP: Adicionando a lista de tradutores do fork

--- scripts/setup.js
+++ scripts/setup.js
@@ -26,6 +26,34 @@
 })(jQuery);
 
 (function ($) {
+   // Load translators 
+    var $translators = $('#translators');
+    var repo_user = 'phpsp';
+    if ( $translators.length ) {
+        var fail = function () {
+            $translators.html('<p>Este projeto não seria possível sem a ajuda de <a href="https://github.com/' + repo_user + '/php-the-right-way/graphs/contributors">nossos contribuintes</a> no GitHub.</p>');
+        };
+        $.ajax({
+            cache: false,
+            dataType: 'jsonp',
+            timeout: 3000,
+            type: 'GET',
+            url: 'https://api.github.com/repos/' + repo_user + '/php-the-right-way/contributors'
+        }).done(function (data) {
+            if ( data.data && data.data.length ) {
+                var $ul = $('<ul></ul>'), dataLength = data.data.length;
+                for ( var i = 0; i < dataLength; i++ ) {
+                    $ul.append(['<li><a href="https://github.com/', data.data[i].login, '" target="_blank">', data.data[i].login, '</a></li>'].join(''));
+                }
+                $translators.html($ul);
+            } else {
+                fail();
+            }
+        }).fail(fail);
+    }
+})(jQuery);
+
+(function ($) {
     //Add current view's highlighting to the navigation
     
     /** helper for highlighting */
